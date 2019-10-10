3b34e11bd2c37628268b91ff808a30f2d9b91f39
gerald <geraldine.antonelli@gmail.com>
2019-10-10 11:20:41 +0100

    fetch is not working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div class="">
+    <h2>Brewdog App</h2>
+    <div class="wrapper">
+      <beers-list :beers='beers'></beers-list>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {evenBus} from './main.js';
+import BeersList from './components/BeersList.vue'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      beers: []
+    }
+  },
+
+  mounted(){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(beers => this.beers = beers)
+
+
+  },
+
   components: {
-    HelloWorld
+    "beers-list": BeersList
   }
+
+
 }
+
+
+
+
+
+
+
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/BeersList.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="">
+    <span>
+      <beers-list v-for="(beer, index) in beers" :beer="beer" :key="index"></beers-list>
+    </span>
+  </div>
+</template>
+
+<script>
+import BeersList from './BeersList.vue';
+
+export default {
+  name:'beers-list',
+  props: ['beers'],
+  components:{
+    "beers-list": BeersList
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
